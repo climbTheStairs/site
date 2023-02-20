591bcc8163894c94011df27d6381d1f04b9b5e26
climbTheStairs <yxtqwf+github@gmail.com>
2023-02-20 02:01:47 -0800

    build.sh: use `tsv template` instead of PHP

--- .build/build.sh
+++ .build/build.sh
@@ -4,8 +4,8 @@ rootdir='/srv/climbthestairs.org'
 builddir="$rootdir/.build"
 medir="$HOME/me"
 
-#tsv template "$builddir/ficls.html.template" \
-#	< "$medir/ficls.tsv" \
-#	> "$rootdir/ficls/index.html"
-php "$builddir/ficls.php" > "$rootdir/ficls/index.html"
+tsv template "$builddir/ficls.html.template" \
+	< "$medir/ficls.tsv" \
+	> "$rootdir/ficls/index.html"
+#php "$builddir/ficls.php" > "$rootdir/ficls/index.html"
 
